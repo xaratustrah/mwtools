ab9a7bf6b8b8d277c4a286369adc3af591ceed01
jepio <jeremi.piotrowski@gmail.com>
2015-01-14 14:07:14 +0100

    Fixed bugs in the script, and rewrote it to work with any mode number.
    TODO: Try to find a fix so that it is not necessary to read the whole
    file into memory at once. Iterating over it is not doable now as I rely
    on splitting by "Mode Number". Maybe all that is needed is to find the
    length of the first "Mode data" and then use that.

--- mws2cols.py
+++ mws2cols.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
-# Convert MWS multi parameteric plots to columns
-#
-# by github.com/jepio 2014
-#
+"""Convert MWS multi parameteric plots to columns
 
+by github.com/jepio 2014"""
+
+from __future__ import print_function, division
 import sys, re
+from itertools import islice
 
-pattern = re.compile("(\d+\.?\d*)")
-file = open(sys.argv[1])
-out = re.finditer(pattern, file.read())
-results = (m.group(1) for m in out)
-results = [r for r in results if r not in ("1", "2")]
-n = int(len(results) / 3)
-for i in range(n):
-    print(" ".join(results[i:i + 3]))
+number_pattern = re.compile("(\d+\.?\d*([eE][+-]\d+)?)")
+
+def parse(run_data):
+    data = re.finditer(number_pattern, run_data)
+    run_number = next(data).group(1)
+    # Once the run number has been removed, every second entry is an R/Q
+    # value.
+    numbers = (match.group(1) for match in data)
+    r_over_q = islice(numbers, 1, None, 2)
+    return (run_number, " ".join(r_over_q,))
+
+file_ = open(sys.argv[1])
+data = file_.read()
+runs = data.split("Mode Number")
+
+for run in runs[1:]:
+    run_number, r_over_q = parse(run)
+    print(run_number, r_over_q)
